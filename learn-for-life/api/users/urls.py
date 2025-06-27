2763bc78cf2f453f30d3a0274819d1a0384e21fd
gal.esm49 <esm.2001@mail.ru>
2025-06-27 15:57:23 +0500

    Update users/urls.py

--- learn-for-life/api/users/urls.py
+++ learn-for-life/api/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import UserRegisterAPIView
+from .views import UserRegisterAPIView 
+from .views import protected_view
 
 urlpatterns = [
     path('register/', UserRegisterAPIView.as_view(), name='register'),
+    path('protected/', protected_view, name='protected'),
 ]
