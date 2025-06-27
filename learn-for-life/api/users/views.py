c415c6cf6d3585fd5b9398f480ef3f776fb1c4a1
gal.esm49 <esm.2001@mail.ru>
2025-06-27 15:57:53 +0500

    Update users/views.py

--- learn-for-life/api/users/views.py
+++ learn-for-life/api/users/views.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserRegisterSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 class UserRegisterAPIView(APIView):
     def post(self, request):
@@ -11,4 +13,13 @@ class UserRegisterAPIView(APIView):
             serializer.save()
             return Response({'message': 'Регистрация прошла успешно'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def protected_view(request):
+    user = request.user
+    return Response({
+        'message': f'Добро пожаловать, {user.username}!',
+        'email': user.email
+    })
 
