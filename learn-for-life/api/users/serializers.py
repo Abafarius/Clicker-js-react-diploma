61f21a0d6f999f5439ce5c3754e66fbefc83724c
gal.esm49 <esm.2001@mail.ru>
2025-06-30 01:39:05 +0500

    Update serializers.py

--- learn-for-life/api/users/serializers.py
+++ learn-for-life/api/users/serializers.py
@@ -4,9 +4,17 @@ from django.contrib.auth.models import User
 class UserRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
+    email = serializers.EmailField(required=True)
+
+class Meta:
+    model = User
+    fields = ['username', 'email', 'password']
+    extra_kwargs = {
+        'email': {'validators': [
+            serializers.UniqueValidator(queryset=User.objects.all(), message="Почта уже используется")
+        ]}
+    }
+
 
     def create(self, validated_data):
         user = User(
@@ -16,3 +24,10 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+    
+    
+    def validate_password(self, value):
+        if len(value) < 6:
+            raise serializers.ValidationError("Пароль должен содержать минимум 6 символов.")
+        return value
+
