18773f1efbfc5e3a6ba2df47dcbdb1a43dce7459
gal.esm49 <esm.2001@mail.ru>
2025-07-01 01:28:33 +0500

    Update App.jsx

--- learn-for-life/frontend/src/App.jsx
+++ learn-for-life/frontend/src/App.jsx
@@ -1,29 +1,35 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/auth/Login";
-import Register from "./components/auth/Register";
-import GameWrapper from "./components/GameWrapper";
-import PrivateRoute from "./components/PrivateRoute";
-import Welcome from "./components/Welcome";
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import GameWrapper from './components/GameWrapper';
+import PrivateRoute from './components/PrivateRoute';
+import Welcome from './components/Welcome';
+import { WipeProvider } from './components/WipeContext';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-
-        {/* Защищённый маршрут для игры */}
-        <Route
-          path="/game"
-          element={
-            <PrivateRoute>
-              <GameWrapper />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
+      <WipeProvider>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/game"
+            element={
+              <PrivateRoute>
+                <GameWrapper />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </WipeProvider>
     </Router>
   );
 }
